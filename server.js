7ec52b20be02faf9487ac889dbcadf10064e1f36
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-05-17 02:39:35 -0400

    fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv/config');
 
 // DB CONNECTION
 mongoose.connect(
